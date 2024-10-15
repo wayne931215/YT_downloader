55b6431ee7ef2990b59bf456da67054307cfe66e
wayne931215 <william100263@gmail.com>
2024-10-16 01:32:37 +0800

    Downloader 1.0 finished Significant change: When downloading a playlist, first create a folder named by the title of the playlist and then download the videos in that folder

---
+++ downloader.py
@@ -0,0 +1,83 @@
+from pytubefix import YouTube
+from pytubefix import Channel
+from pytubefix import Playlist
+from pytubefix.cli import on_progress
+import os
+
+### Download a video with highest resolution
+def DownloadAsMP4(url, path):
+    try:
+        yt = YouTube(url, on_progress_callback = on_progress)
+        print(yt.title)
+        yt.streams.get_highest_resolution().download(path)
+    except Exception as e:
+        print(e)
+        
+    
+### Download a video as .mp3
+def DownloadAsMP3(url, path):
+    try:
+        yt = YouTube(url, on_progress_callback = on_progress)
+        print(yt.title)
+        ys = yt.streams.get_audio_only()
+        ys.download(path, mp3=True)
+    except Exception as e:
+        print(e)
+    
+### Download a whole playlist
+def DownloadPlaylist(url, path):
+    try:
+        pl = Playlist(url)
+        title = pl.title
+        if not os.path.exists(title):
+            os.mkdir(title)
+        path = os.path.join(path, title)
+        print(str(len(pl.videos)) + ' video(s) in the playlist')
+        for video in pl.videos:
+            print(video.title)
+            video.streams.get_highest_resolution().download(path)
+    except Exception as e:
+        print(e)
+            
+    
+### Download all video from a channel
+def DownloadAllVideo(url, path):
+    try:
+        c = Channel(url)
+        print(f'Downloading videos from channel: {c.channel_name}')
+        print(str(len(c.videos)) + ' video(s) in the channel')
+        for video in c.videos:
+            print(video.title)
+            video.streams.get_highest_resolution().download(path)
+    except Exception as e:
+        print(e)
+        
+def show():
+    print('Here is a downloader supporting following four functions:')
+    print('1. Download a video with highest resolution')
+    print('2. Download a video as a .mp3 file')
+    print('3. Download the whole playlist')
+    print('4. Download all video from a channel')
+    ins = int(input('Enter 1~4 to do what you want: '))
+    return ins
+    
+instruction = show()
+url = ''
+### Remember to change it if you want to download the video in other folders
+path = os.path.dirname(__file__)
+
+if instruction == 1:
+    url = input('Enter the url of the video:\n')
+    DownloadAsMP4(url, path)
+elif instruction == 2:
+    url = input('Enter the url of the video:\n')
+    DownloadAsMP3(url, path)
+elif instruction == 3:
+    url = input('Enter the url of the playlist:\n')
+    DownloadPlaylist(url, path)
+elif instruction == 4:
+    url = input('Enter the url of the channel\'s homepage:\n')
+    DownloadAllVideo(url, path)
+
+
+    
